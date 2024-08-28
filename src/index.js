0cdc4d3b1f80a8fefdc180433e930fa21a8e34dc
serjobor <borisov_sergey2002@mail.ru>
2024-08-28 13:45:33 +0400

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    return +([...('' + Math.abs(n))].reverse().join(''));
+    // const arr = [...('' + Math.abs(n))];
+    // const result = [];
+
+    // for (const key in arr){
+    //     result.unshift(arr[key]);
+    // }
+
+    // return +result.join('');
 }
